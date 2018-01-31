5174a361cefd83cdee40384b16da4ccba7c48435
blueberrypineapple <35980495+blueberrypineapple@users.noreply.github.com>
2018-01-30 22:56:34 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+#pip install nltk
+import requests
+from bs4 import BeautifulSoup
+import re
+from nltk.tokenize import RegexpTokenizer
+import nltk
+#nltk.download('popular')--> debugging success
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+
+
+
+
+def plot_word_freq(url):
+    """Takes a url (from Project Gutenberg) and plots a word frequency
+    distribution"""
+    # Make the request and check object type
+    r = requests.get(url)
+    # Extract HTML from Response object and print
+    html = r.text
+    # Create a BeautifulSoup object from the HTML
+    soup = BeautifulSoup(html, "html5lib")
+    # Get the text out of the soup and print it
+    text = soup.get_text()
+    # Create tokenizer
+    tokenizer = RegexpTokenizer('\w+')
+    # Create tokens
+    tokens = tokenizer.tokenize(text)
+    # Initialize new list
+    words = []
+    # Loop through list tokens and make lower case
+    for word in tokens:
+        words.append(word.lower())
+    # Get English stopwords and print some of them
+    sw = nltk.corpus.stopwords.words('english')
+    # Initialize new list
+    words_ns = []
+    # Add to words_ns all words that are in words but not in sw
+    for word in words:
+        if word not in sw:
+            words_ns.append(word)
+    # Create freq dist and plot
+    freqdist1 = nltk.FreqDist(words_ns)
+    freqdist1.plot(25)
+
+user_url = input("Please enter a valid book url: ")
+plot_word_freq(user_url)
